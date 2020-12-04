77965bd0da0c4b35a530a0adff21bf8a5ebb7e86
Ed Ag <eduxx77@gmail.com>
2020-12-03 20:55:39 -0600

    Added simple rain generation to comply with professor especifications

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 /*jshint esversion: 6 */
 
 var scene, camera, renderer, controls;
+const CLOUD_RADIUS = 8;
+const WATER_LEVEL = 4;
+const ZOOM = 20;
+const TERRAIN_SIZE = 40;
 
 function generateCubeGeometry() {
   const boxUnit = 1;
   const boxWidth = boxUnit;
   const boxHeight = boxUnit;
   const boxDepth = boxUnit;
+  return new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+}
+
+function generateRainGeometry() {
+  const boxUnit = 1;
+  const boxWidth = boxUnit/4;
+  const boxHeight = boxUnit;
+  const boxDepth = boxUnit/4;
   return new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 }
 
@@ -44,6 +56,26 @@ const WaterMaterials = [
   new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0, transparent: true})),
 ];
 
+const rainColor = hsl(5 / 8, 1, 0.5);
+const RainMaterials = [
+  new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0.5, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0.5, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0.5, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0.5, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0.5, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: waterColor, opacity: 0.5, transparent: true})),
+];
+
+const cloudColor = hsl(0, 1, 1);
+const CloudMaterials = [
+  new THREE.MeshPhongMaterial(({color: cloudColor, opacity: 1, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: cloudColor, opacity: 1, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: cloudColor, opacity: 1, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: cloudColor, opacity: 1, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: cloudColor, opacity: 1, transparent: true})),
+  new THREE.MeshPhongMaterial(({color: cloudColor, opacity: 1, transparent: true})),
+];
+
 function makeInstance(geometry, x, y = 0, z = 0, material = "dirt") {
   var materials = DirtMaterials;
   switch (material) {
@@ -55,6 +87,9 @@ function makeInstance(geometry, x, y = 0, z = 0, material = "dirt") {
       break;
     case "water":
       materials = WaterMaterials;
+      break;
+    case "cloud":
+      materials = CloudMaterials;
       break;
     default:
 
@@ -69,6 +104,42 @@ function makeInstance(geometry, x, y = 0, z = 0, material = "dirt") {
   return cube;
 }
 
+
+var rainDataX = new Array();
+var rainDataY = new Array();
+var rainDataZ = new Array();
+var rainData = new Array();
+var cloudHeight = 0;
+function generateCloud(height, x, y) {
+  const cubeGeometry = generateCubeGeometry();
+  const rainGeometry = generateRainGeometry();
+  cloudHeight = height;
+  x = x - CLOUD_RADIUS/2;
+  y = y - CLOUD_RADIUS/2;
+  for (var i = 0; i < CLOUD_RADIUS; i++) {
+    for (var j = 0; j < CLOUD_RADIUS; j++) {
+      const variableheight = Math.floor(Math.random() * height);
+      const coorX = x+i;
+      const coorY = height;
+      const coorZ = y+j;
+      makeInstance(cubeGeometry, coorX, coorY, coorZ, "cloud");
+      rainDataX[i*CLOUD_RADIUS + j] = coorX;
+      rainDataY[i*CLOUD_RADIUS + j] = coorY - variableheight;
+      rainDataZ[i*CLOUD_RADIUS + j] = coorZ;
+      // var drop
+      rainData[i*CLOUD_RADIUS + j] = new THREE.Mesh(rainGeometry, RainMaterials);
+      // drop = rainData[i*CLOUD_RADIUS + j];
+
+      scene.add(rainData[i*CLOUD_RADIUS + j]);
+
+      rainData[i*CLOUD_RADIUS + j].position.x = coorX;
+      rainData[i*CLOUD_RADIUS + j].position.y = coorY - variableheight;
+      rainData[i*CLOUD_RADIUS + j].position.z = coorZ;
+    }
+  }
+}
+
+var terrainData = new Array();
 function generateTerrain(width, length) {
   function isEdgeCube(x, y, z, maxY){
     // return true;
@@ -94,12 +165,11 @@ function generateTerrain(width, length) {
     return true;
   }
 
-  const ZOOM = 20;
-  const WATER_LEVEL = 9;
   const cubeGeometry = generateCubeGeometry();
   noise.seed(Math.random());
 
-  var terrainData = new Array();
+  var maxHeight = 0;
+
   //march cube through all topological cubes
   for(var x = 0; x < width; x++) {
     terrainData[x] = new Array();
@@ -109,6 +179,7 @@ function generateTerrain(width, length) {
       const genValue = noise.perlin2(x / ZOOM, z / ZOOM);
       const y = Math.floor(Math.abs(genValue) * ZOOM);
       terrainData[x][z] = y;
+      maxHeight = Math.max(maxHeight, y);
     }
   }
 
@@ -128,6 +199,8 @@ function generateTerrain(width, length) {
       }
     }
   }
+
+  generateCloud(maxHeight+3, 0, 0);
 }
 
 function main() {
@@ -150,12 +223,10 @@ function main() {
     // const light = new THREE.DirectionalLight(color, intensity);
     // light.position.set(-1, 2, 4);
     const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-    scene.add( light );
+    scene.add(light);
     scene.add(light);
   }
-
-  const size = 40;
-  generateTerrain(size,size);
+  generateTerrain(TERRAIN_SIZE,TERRAIN_SIZE);
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -170,6 +241,7 @@ function main() {
   }
 
   function render(time) {
+    // console.log("frame")
     time *= 0.001;
 
     if (resizeRendererToDisplaySize(renderer)) {
@@ -178,9 +250,31 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
+    const rainSpeed = 0.5;
+    for (var i = 0; i < rainDataX.length; i++) {
+      // console.log("loop");
+      if(i == 0){
+        // console.log("before");
+        // console.log(rainDataY[i]);
+      }
+      rainDataY[i] = rainDataY[i] - (rainSpeed);
+      rainData[i].position.y = rainDataY[i];
+      // const rainX = CLOUD_RADIUS - rainDataX[i];
+      // const rainZ = CLOUD_RADIUS - rainDataZ[i];
+      // const terrainHeight = terrainData[rainX][rainZ];
+      if(rainDataY[i] <= 0 || rainDataY[i] <= WATER_LEVEL){
+        rainDataY[i] = cloudHeight;
+      }
+    }
+
+    // rainData.forEach((rain, i) => {
+    //   const speed = 1 + i * 0.1;
+    //   rain.position.y -= speed;
+    // });
+
+
     renderer.render(scene, camera);
     controls.update();
-
     requestAnimationFrame(render);
   }
 
